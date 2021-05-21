ce763eb8e1c27a0500ac90b832c7046e5d7511bd
zifeng <mg20220214@smail.nju.edu.cn>
2021-05-21 15:12:43 +0800

    In IHEP, in the next few days, I will trun to eletronics

---
+++ analysis_waveform.h
@@ -0,0 +1,80 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Fri May 21 14:30:38 2021 by ROOT version 6.22/06
+// from TTree tree_waveform_CH1/A tree save channel CH1 wave form data and size of each measurment
+// found on file: CH1_test.root
+//////////////////////////////////////////////////////////
+
+#ifndef analysis_waveform_h
+#define analysis_waveform_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TSelector.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+
+// Headers needed by this particular selector
+
+
+class analysis_waveform : public TSelector {
+public :
+   TTreeReader     fReader;  //!the tree reader
+   TTree          *fChain = 0;   //!pointer to the analyzed TTree or TChain
+
+   // Readers to access the data (delete the ones you do not need).
+   TTreeReaderValue<Int_t> CH1_size = {fReader, "CH1_size"};
+   TTreeReaderArray<Double_t> CH1_time = {fReader, "CH1_time"};
+   TTreeReaderArray<Int_t> CH1_voltage = {fReader, "CH1_voltage"};
+
+
+   analysis_waveform(TTree * /*tree*/ =0) { }
+   virtual ~analysis_waveform() { }
+   virtual Int_t   Version() const { return 2; }
+   virtual void    Begin(TTree *tree);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual void    Init(TTree *tree);
+   virtual Bool_t  Notify();
+   virtual Bool_t  Process(Long64_t entry);
+   virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
+   virtual void    SetOption(const char *option) { fOption = option; }
+   virtual void    SetObject(TObject *obj) { fObject = obj; }
+   virtual void    SetInputList(TList *input) { fInput = input; }
+   virtual TList  *GetOutputList() const { return fOutput; }
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   ClassDef(analysis_waveform,0);
+
+};
+
+#endif
+
+#ifdef analysis_waveform_cxx
+void analysis_waveform::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the reader is initialized.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   fReader.SetTree(tree);
+}
+
+Bool_t analysis_waveform::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+
+#endif // #ifdef analysis_waveform_cxx
