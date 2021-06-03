5b8eb07d1d15994f4a5fec5f4971cd3947f832a6
Zifeng Xu <zifeng.xu@cern.ch>
2021-06-03 19:50:09 +0800

    Test alll function in Zichangge's code, they work fine

--- my_oscilloscope_package/Oscilloscope.py
+++ my_oscilloscope_package/Oscilloscope.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # Author: Zifeng Xu, Changge Zi.
 # Email: zifeng.xu@cern.ch
+# Note:1, How to detect muon decay events in our lab?
+#      2, Only sample the channel you already setup
 import pyvisa
 import time
 
 import numpy as np
 import matplotlib.pyplot as plt
+from pyvisa.constants import DataWidth
 
 
 class Oscilloscope():
@@ -13,114 +16,131 @@ class Oscilloscope():
     """
     This class include method of initialize and setup the oscilloscope via usb.
     And some methods to measure the waveform and get the data.
-    list_channels should like CH1,CH2,CH3
+  
     """
 
-    def __init__(self, list_channels='CH1', resource_name='USB0::0x0699::0x03C7::C011248::INSTR'):
+    def __init__(self, resource_name='USB0::0x0699::0x03C7::C011248::INSTR'):
         # enable libvisa support. Only available on Mac and Windows
         self.resource_manager = pyvisa.ResourceManager()
         # enable full python pyvisa
         # self.resource_manager=pyvisa.ResourceManager("@py")
-        self.list_channels= list_channels.split(',')
-        self.inst= self.resource_manager.open_resource(
+        self.inst = self.resource_manager.open_resource(
             resource_name=resource_name)
         self.inst.timeout = 10000  # ms
         self.inst.encoding = 'latin_1'
         self.inst.read_termination = '\n'
         self.inst.write_termination = '\n'
-        self.inst.write('*cls') # clear ESR
-        self.inst.write('header OFF') # disable attribute echo in replies
-        print(self.inst.query('*idn?'))
-        # Create dictionary for 
-        self.dic_waveform={}
-        for channel in self.list_channels:
-            self.dic_waveform[channel]=[]
-            print("Create list for {0}".format(channel))
-
-
-    def Sampling(self, M = 8192):     #M is the number of sampling points
-        # default setup
-        self.inst.write('*rst')
-        #t1 = time.perf_counter()
-        r = self.inst.query('*opc?') # sync
-        #t2 = time.perf_counter()
-        #print('reset time: {} s'.format(t2 - t1))
-
+        self._channel='DEFAULT'
+        self.inst.write('*cls')  # clear ESR
+        self.inst.write('header OFF')  # disable attribute echo in replies
+        print(self.inst.query('*idn?')) 
         # autoset
-        self.inst.write('autoset EXECUTE')
-        #t3 = time.perf_counter()
-        r = self.inst.query('*opc?')
-        #t4 = time.perf_counter()
-        #print('autoset time: {} s'.format(t4 - t3))
+        # self.Autoset()
+
+       
+    def Sampling(self, sammple_channels="CH1", M=2000):  # M is the number of sampling points
+        list_channels=sammple_channels.split(',')
+        # Prepare sampling
+        # default setup
+        # self.inst.write('*rst')
+        r = self.inst.query('*opc?')  # sync
 
         # acquisition
-        self.inst.write('acquire:state OFF') # stop
-        self.inst.write('acquire:stopafter SEQUENCE;state ON') # single
-        #t5 = time.perf_counter()
+        self.inst.write('acquire:state OFF')  # stop
+        self.inst.write('acquire:stopafter SEQUENCE;state ON')  # single
         r = self.inst.query('*opc?')
-        #t6 = time.perf_counter()
-        #print('acquire time: {} s'.format(t6 - t5))
 
         # curve configuration
-        self.inst.write('data:encdg SRIBINARY') # signed integer
-        self.inst.write('data:source CH1')
-        self.inst.write('data:start 1')
-        acq_record = int(M)      #self.inst.query('horizontal:recordlength?')
-        self.inst.write('data:stop {}'.format(acq_record))
-        self.inst.write('wfmoutpre:byt_nr 1') # 1 byte per sample
+        self.inst.write('data:encdg SRIBINARY')  # signed integer
+        acq_record = int(M)  # self.inst.query('horizontal:recordlength?')
+        self.inst.write('data:stop {0}'.format(acq_record))
+        self.inst.write('wfmoutpre:byt_nr 1')  # 1 byte per sample
 
-        # data query
-        #t7 = time.perf_counter()
-        bin_wave = self.inst.query_binary_values('curve?', datatype='b', container=np.array)
-        #t8 = time.perf_counter()
-        #print('transfer time: {} s'.format(t8 - t7))
+        dic_waveforms={}
+        for channel in list_channels:
+            self.SetChannel(channel)
+            self.inst.write('data:source {0}'.format(self._channel))
+            # data query
+            # send message in binary form
+            bin_wave = self.inst.query_binary_values('curve?', datatype='b', container=np.array)
+            print(bin_wave)
+            
+            # retrieve scaling factors
+            wfm_record = int(self.inst.query('wfmoutpre:nr_pt?'))
+            pre_trig_record = int(self.inst.query('wfmoutpre:pt_off?'))
+            t_scale = float(self.inst.query('wfmoutpre:xincr?'))
+            # sub-sample trigger correction
+            t_sub = float(self.inst.query('wfmoutpre:xzero?'))
+            v_scale = float(self.inst.query('wfmoutpre:ymult?'))  # volts / level
+            v_off = float(self.inst.query('wfmoutpre:yzero?'))  # reference voltage
+            # reference position (level)
+            v_pos = float(self.inst.query('wfmoutpre:yoff?'))
 
-        # retrieve scaling factors
-        wfm_record = int(self.inst.query('wfmoutpre:nr_pt?'))
-        pre_trig_record = int(self.inst.query('wfmoutpre:pt_off?'))
-        t_scale = float(self.inst.query('wfmoutpre:xincr?'))
-        t_sub = float(self.inst.query('wfmoutpre:xzero?')) # sub-sample trigger correction
-        v_scale = float(self.inst.query('wfmoutpre:ymult?')) # volts / level
-        v_off = float(self.inst.query('wfmoutpre:yzero?')) # reference voltage
-        v_pos = float(self.inst.query('wfmoutpre:yoff?')) # reference position (level)
+            # error checking
+            r = int(self.inst.query('*esr?'))
+            r = self.inst.query('allev?').strip()
 
-        # error checking
-        r = int(self.inst.query('*esr?'))
-        #print('event status register: 0b{:08b}'.format(r))
-        r = self.inst.query('allev?').strip()
-        #print('all event messages: {}'.format(r))
+            # create scaled vectors
+            # horizontal (time)
+            total_time = t_scale * wfm_record
+            t_start = (-pre_trig_record * t_scale) + t_sub
+            t_stop = t_start + total_time
+            scaled_time = np.linspace(
+                t_start, t_stop, num=wfm_record, endpoint=False)
 
-        # create scaled vectors
-        # horizontal (time)
-        total_time = t_scale * wfm_record
-        t_start = (-pre_trig_record * t_scale) + t_sub
-        t_stop = t_start + total_time
-        scaled_time = np.linspace(t_start, t_stop, num=wfm_record, endpoint=False)
+            # vertical (voltage)
+            # data type conversion
+            unscaled_wave = np.array(bin_wave, dtype='double')
+            scaled_wave = (unscaled_wave - v_pos) * v_scale + v_off
 
-        # vertical (voltage)
-        unscaled_wave = np.array(bin_wave, dtype='double') # data type conversion
-        scaled_wave = (unscaled_wave - v_pos) * v_scale + v_off
         return scaled_time, scaled_wave
 
-    def MeasureFreq(self):    #MEASUre Frequency
-        self.inst.write(':MEASUrement:IMMed:SOUrce1 CH1')
+    def MeasureFreq(self):  # MEASUre Frequency
+        self.inst.write(':MEASUrement:IMMed:SOUrce1 {0}'.format(self._channel))
         self.inst.write(':MEASUrement:IMMed:TYPe FREQuency')
         return (self.inst.query(':MEASUrement:IMMed:VALue?'))
 
-    def MeasureAmpl(self):    #MEASUre Amplitude
-        self.inst.write(':MEASUrement:IMMed:SOUrce1 CH1')
+    def MeasureAmpl(self):  # MEASUre Amplitude
+        self.inst.write(':MEASUrement:IMMed:SOUrce1 {0}'.format(self._channel))
         self.inst.write(':MEASUrement:IMMed:TYPe AMPlitude')
         return (self.inst.query(':MEASUrement:IMMed:VALue?'))
 
-    def MeasureHigh(self):    #MEASUre Low
-        self.inst.write(':MEASUrement:IMMed:SOUrce1 CH1')
+    def MeasureHigh(self):  # MEASUre Low
+        self.inst.write(':MEASUrement:IMMed:SOUrce1 {0}'.format(self._channel))
         self.inst.write(':MEASUrement:IMMed:TYPe HIGH')
         return (self.inst.query(':MEASUrement:IMMed:VALue?'))
 
-    def MeasureLow(self):    #MEASUre Low
-        self.inst.write(':MEASUrement:IMMed:SOUrce1 CH1')
+    def MeasureLow(self):  # MEASUre Low
+        self.inst.write(':MEASUrement:IMMed:SOUrce1 {0}}'.format(self._channel))
         self.inst.write(':MEASUrement:IMMed:TYPe LOW')
         return (self.inst.query(':MEASUrement:IMMed:VALue?'))
+
+    def SetChannel(self, channel):
+        """
+        Set channel for Measure and Sampling
+        example, "CH1" "CH2"
+        """
+
+        # Check if channel is legal
+        if(channel == 'DEFAULT'):
+            print("Oscilloscop::WARNING the channel is set to DEFAULT now, no measure of data-save is proceeded")
+        if(channel == 'CH1'):
+            self._channel=channel       
+            print("Set CH1")
+        elif(channel == 'CH2'):
+            self._channel=channel
+            print("Set CH2")
+        elif(channel =='CH3'):
+            self._channel=channel
+            print("Set CH3")
+        elif(channel=='CH4'):
+            self._channel=channel
+            print("Set CH4")
+        else:
+            self._channel="DEFAULT"
+            print("The select channel should be one of CH1, CH2, CH3, CH4\nSet DEFUALT")
+
+
 
     def Autoset(self):
         self.inst.write('autoset EXECUTE')
@@ -128,4 +148,5 @@ class Oscilloscope():
     # disconnect
     def close(self):
         self.inst.close()
-        self.rm.close()
+        self.resource_manager.close()
+
--- my_oscilloscope_package/test_Loop.py
+++ my_oscilloscope_package/test_Loop.py
@@ -1,3 +1,15 @@
-import Oscilloscope
+from weakref import WeakValueDictionary
+from Oscilloscope import Oscilloscope
 import pyvisa
 import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+for i in range(0,10):
+    Scope=Oscilloscope()
+    wavetime, waveform=Scope.Sampling("CH1,CH2,CH3")
+    print("time: {0}\nwaveform: {1}".format(wavetime, waveform))
+    # plt.plot(wavetime,waveform)
+    # plt.show()
+    time.sleep(0.5)
