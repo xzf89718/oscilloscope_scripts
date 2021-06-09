7008a45cb9b7b11a7f18cc40aae8bdb8ebf87288
Zifeng Xu <zifeng.xu@cern.ch>
2021-06-09 14:48:29 +0800

    Small change in analysis_waveform_data.py

--- analysis_waveform_data.py
+++ analysis_waveform_data.py
@@ -16,20 +16,22 @@ def RunSelector(input_TFile, channels='CH1,CH2,CH3', output_filename='output_fil
             tree=input_TFile.Get("tree_waveform_{0}".format(_channel))
         except:
             print("tree_waveform_{0} is not exist".format(_channel))
-        tree.Process("single_channel_selector.C+", "output_filename_{0}.root".format(_channel))
+        tree.Process("single_channel_selector.C+", "{1}_{0}.root".format(_channel, output_filename))
 
     return 0
 
 def RunAnalysis(channels='CH1,CH2,CH3', input_filename='output_filename', min_trig_level_cut=0.0):
 
-    dic_tree
+    dic_tree = {}
     try:
         ROOT.TFile.Open("".format())
+    except:
+        print("hello")
     # Check the length of these trees
 
     return 0
 
-input_TFile=ROOT.TFile.Open("1000_square_waveform.root", "READ")
+input_TFile=ROOT.TFile.Open("channel1_trig_n4p00.root", "READ")
 
-RunSelector(input_TFile, "CH1,CH2")
-RunAnalysis(channels='CH1,CH2', input_filename='output_filename')
+RunSelector(input_TFile, "CH1,CH2,CH3", output_filename='channel1_trig_n4p00')
+# RunAnalysis(channels='CH1,CH2', input_filename='output_filename')
