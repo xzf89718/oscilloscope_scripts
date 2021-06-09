6a2988bf6be31d2fca952e1bf09fbcf2b1970e03
zifeng <zifeng.xu@cern.ch>
2021-06-09 09:42:37 +0800

    add analysis_waveform_data.py

---
+++ analysis_waveform_data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Author: Zifeng Xu
+# Email: zifeng.xu@cern.ch
+# Usage: Use this interface to get charge and max voltage in waveform
+
+import numpy as np
+# import matplotlib.pyplot as plt
+import ROOT
+
+def RunSelector(input_TFile, channels='CH1,CH2,CH3', output_filename='output_filename'):
+
+    list_channel = channels.split(',')
+
+    for _channel in list_channel:
+        try:
+            tree=input_TFile.Get("tree_waveform_{0}".format(_channel))
+        except:
+            print("tree_waveform_{0} is not exist".format(_channel))
+        tree.Process("single_channel_selector.C+", "output_filename_{0}.root".format(_channel))
+
+    return 0
+
+def RunAnalysis(channels='CH1,CH2,CH3', input_filename='output_filename', min_trig_level_cut=0.0):
+
+    dic_tree
+    try:
+        ROOT.TFile.Open("".format())
+    # Check the length of these trees
+
+    return 0
+
+input_TFile=ROOT.TFile.Open("1000_square_waveform.root", "READ")
+
+RunSelector(input_TFile, "CH1,CH2")
+RunAnalysis(channels='CH1,CH2', input_filename='output_filename')
